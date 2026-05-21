25280b48d5d349a485a9e92c41199892a03ba06d
a-Bouchez <Alexis.starcenbaum@gmail.com>
2026-05-21 14:55:03 -0300

    Fixes releases folders

---
+++ InventoryPatches/InventoryMenuHoverPatch.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using StardewValley;
+using StardewValley.Menus;
+using StardewValley.Tools;
+
+namespace PanSlotMod.InventoryPatches
+{
+    [HarmonyPatch(typeof(InventoryMenu), nameof(InventoryMenu.hover))]
+    public class InventoryMenuHoverPatch
+    {
+        private static Pan _savedPan;
+
+        public static void Prefix(InventoryMenu __instance)
+        {
+            if (__instance.actualInventory != Game1.player.Items) return;
+
+            int panIndex = PanSlotState.PanSlotIndex;
+            if (__instance.actualInventory.Count > panIndex)
+            {
+                _savedPan = PanSlotState.GetPan();
+                __instance.actualInventory[panIndex] = null;
+            }
+        }
+
+        public static void Postfix(InventoryMenu __instance)
+        {
+            if (__instance.actualInventory != Game1.player.Items)
+            {
+                _savedPan = null;
+                return;
+            }
+
+            int panIndex = PanSlotState.PanSlotIndex;
+            if (__instance.actualInventory.Count > panIndex)
+                __instance.actualInventory[panIndex] = _savedPan;
+
+            _savedPan = null;
+        }
+    }
+}
