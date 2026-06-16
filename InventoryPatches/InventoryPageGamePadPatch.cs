2792eef5823de929024898804225f1c015c8c9c2
a-Bouchez <Alexis.starcenbaum@gmail.com>
2026-06-16 12:01:34 -0300

    Adds Controller patch.

---
+++ InventoryPatches/InventoryPageGamePadPatch.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using Microsoft.Xna.Framework.Input;
+using StardewValley;
+using StardewValley.Menus;
+
+namespace PanSlotMod
+{
+    [HarmonyPatch(typeof(InventoryPage), nameof(InventoryPage.receiveGamePadButton))]
+    public class InventoryPageGamePadPatch
+    {
+        public static bool Prefix(Buttons button, InventoryPage __instance)
+        {
+            if (button != Buttons.Back)
+                return true;
+
+            var organizeButton = AccessTools.Field(typeof(InventoryPage), "organizeButton").GetValue(__instance);
+            if (organizeButton == null)
+                return true;
+
+            var items = Game1.player.Items;
+            if (items == null || items.Count <= PanSlotState.PanSlotIndex)
+                return true;
+
+            var pan = items[PanSlotState.PanSlotIndex];
+            items.RemoveAt(PanSlotState.PanSlotIndex);
+
+            ItemGrabMenu.organizeItemsInList(items);
+
+            items.Insert(PanSlotState.PanSlotIndex, pan);
+            Game1.playSound("Ship", null);
+
+            return false;
+        }
+    }
+}
